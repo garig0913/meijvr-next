91a3e1cd0a107e1dd75b2658407f9993fc5678de
garig0913 <90028734+garig0913@users.noreply.github.com>
2021-12-15 11:05:09 +0800

    asdasd

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,134 +1,116 @@
+import { useState } from "react";
 import YouTube from "react-youtube";
 var getYouTubeID = require("get-youtube-id");
 import Slider from "../components/Slider/Slider2";
 import Card from "../components/Card/index";
 
 const Product = (props) => {
-   const cards = [
-      <Card />,
-      <Card />,
-      <Card />,
-      <Card />,
-      <Card />,
-      <Card />,
-      <Card />,
-      <Card />,
-   ];
-   const detail = [
-      {
-         title: "Tooluurin zagvar",
-         value: "LXLC",
-      },
-      {
-         title: "Tooluurin zagvar",
-         value: "LXLC",
-      },
-      {
-         title: "Tooluurin zagvar",
-         value: "LXLC",
-      },
-      {
-         title: "Tooluurin zagvar",
-         value: "LXLC",
-      },
-      {
-         title: "Tooluurin zagvar",
-         value: "LXLC",
-      },
-      {
-         title: "Tooluurin zagvar",
-         value: "LXLC",
-      },
-      {
-         title: "Tooluurin zagvar",
-         value: "LXLC",
-      },
-   ];
+  const cards = [<Card />, <Card />, <Card />, <Card />, <Card />, <Card />, <Card />, <Card />];
+  const detail = [
+    {
+      title: "Tooluurin zagvar",
+      value: "LXLC",
+    },
+    {
+      title: "Tooluurin zagvar",
+      value: "LXLC",
+    },
+    {
+      title: "Tooluurin zagvar",
+      value: "LXLC",
+    },
+    {
+      title: "Tooluurin zagvar",
+      value: "LXLC",
+    },
+    {
+      title: "Tooluurin zagvar",
+      value: "LXLC",
+    },
+    {
+      title: "Tooluurin zagvar",
+      value: "LXLC",
+    },
+    {
+      title: "Tooluurin zagvar",
+      value: "LXLC",
+    },
+  ];
 
-   const images = [
-      "https://images.unsplash.com/photo-1546455643-312811da2332?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=636&q=80",
-      "https://images.unsplash.com/photo-1521985068534-cec90327c93b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
-      "https://images.unsplash.com/photo-1513323813850-c7318e3efc71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
-      "https://images.unsplash.com/photo-1612171709946-7fc1298a5a53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80",
-   ];
-   return (
-      <>
-         <div id="topMargin" className="mx-auto h-96 mb-40">
-            <h1 className="text-sm text-blue-500 text-normal ml-2 mb-4">
-               BUTEEGDEHUUN-USNII TOOLUUR-ENGIIIN TOOLUUR
-            </h1>
-            <div id="mainDiv" className=" gap-8 grid grid-cols-2">
-               <div className="">
+  const images = [
+    "https://images.unsplash.com/photo-1546455643-312811da2332?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=636&q=80",
+    "https://images.unsplash.com/photo-1521985068534-cec90327c93b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    "https://images.unsplash.com/photo-1513323813850-c7318e3efc71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    "https://images.unsplash.com/photo-1612171709946-7fc1298a5a53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80",
+  ];
+
+  const [currentImage, setCurrentImage] = useState(images[0]);
+  return (
+    <>
+      <div id="topMargin" className="mx-auto h-96 mb-40">
+        <h1 className="text-sm text-blue-500 text-normal ml-2 mb-4">
+          BUTEEGDEHUUN-USNII TOOLUUR-ENGIIIN TOOLUUR
+        </h1>
+        <div id="mainDiv" className=" gap-8 grid grid-cols-2">
+          <div className="">
+            <img className="w-full mb-2" id="mainImg" src={currentImage} alt="abc" />
+            <div id="imgGrid" className="grid lg:grid-cols-4 md:grid-cols-3 gap-1">
+              {images.map((data) => {
+                return (
                   <img
-                     className="w-full mb-2"
-                     id="mainImg"
-                     src="https://images.unsplash.com/photo-1622915904739-efd69cd8c519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
-                     alt="abc"
+                    className="lg:h-32 md:h-24"
+                    onClick={() => setCurrentImage(data)}
+                    id="smallImg"
+                    src={data}
+                    alt="abc"
                   />
-                  <div
-                     id="imgGrid"
-                     className="grid lg:grid-cols-4 md:grid-cols-3 gap-1"
-                  >
-                     {images.map((data) => {
-                        return (
-                           <img
-                              className="lg:h-32 md:h-24"
-                              onClick={() => console.log(data)}
-                              id="smallImg"
-                              src={data}
-                              alt="abc"
-                           />
-                        );
-                     })}
-                  </div>
-               </div>
-               <div className="flex flex-col justify-between">
-                  <div className="w-5/6 mx-auto">
-                     <h1 className="text-2xl mb-3">
-                        DN 50 huiten usnii tooluur
-                     </h1>
-                     {detail.map((data) => {
-                        return (
-                           <p className="font-bold text-black text-base">
-                              {data.title}:
-                              <span className="font-light">
-                                 {"  " + data.value}
-                              </span>
-                           </p>
-                        );
-                     })}
-                  </div>
-                  <YouTube
-                     className="w-11/12 mx-auto h-80"
-                     videoId={getYouTubeID(
-                        "https://www.youtube.com/watch?v=Ct6BUPvE2sM"
-                     )}
-                  />
-               </div>
+                );
+              })}
             </div>
-         </div>
-         <Slider comps={cards} length={5} title={"Бүтээгдэхүүн"} />
+          </div>
+          <div className="flex flex-col justify-between">
+            <div className="w-5/6 mx-auto">
+              <h1 className="text-2xl mb-3">DN 50 huiten usnii tooluur</h1>
+              {detail.map((data) => {
+                return (
+                  <p className="font-bold text-black text-base">
+                    {data.title}:<span className="font-light">{"  " + data.value}</span>
+                  </p>
+                );
+              })}
+            </div>
+            <YouTube
+              className="w-11/12 mx-auto h-80"
+              videoId={getYouTubeID("https://www.youtube.com/watch?v=Ct6BUPvE2sM")}
+            />
+          </div>
+        </div>
+      </div>
+      <Slider comps={cards} length={5} title={"Бүтээгдэхүүн"} id="slider" />
 
-         {/* CSS */}
-         <style jsx>{`
-            #topMargin {
-               margin-top: 115px;
-               height: fit-content;
-               width: 79%;
-            }
-            #mainDiv {
-               grid-template-columns: 1fr 1fr;
-            }
-            #mainImg {
-               height: 500px;
-               object-fit: cover;
-            }
-            #smallImg {
-               object-fit: cover;
-            }
-         `}</style>
-      </>
-   );
+      {/* CSS */}
+      <style jsx>{`
+        #topMargin {
+          margin-top: 115px;
+          height: fit-content;
+          width: 79%;
+        }
+        #mainDiv {
+          grid-template-columns: 1fr 1fr;
+        }
+        #mainImg {
+          height: 500px;
+          object-fit: cover;
+        }
+        #smallImg {
+          object-fit: cover;
+        }
+        #slider {
+          margin-bottom: 5rem;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Product;
--- src/sections/landing1/Works.js
+++ src/sections/landing1/Works.js
@@ -15,151 +15,136 @@ import Partner from "../../components/PartnersSlider/Partner";
 import Footer from "../../components/Footer/Footer";
 
 const chero = [<Chero />, <Chero />, <Chero />];
-const cards = [
-   <Card />,
-   <Card />,
-   <Card />,
-   <Card />,
-   <Card />,
-   <Card />,
-   <Card />,
-   <Card />,
-];
+const cards = [<Card />, <Card />, <Card />, <Card />, <Card />, <Card />, <Card />, <Card />];
 const logos = [LogoImg, LogoImg, LogoImg, LogoImg, LogoImg, LogoImg];
 const Works = () => {
-   const [items, setItems] = useState([]);
-   const [activeLink, setActiveLink] = useState("*");
+  const [items, setItems] = useState([]);
+  const [activeLink, setActiveLink] = useState("*");
 
-   useEffect(() => {
+  useEffect(() => {
+    setItems(designWorks1);
+  }, []);
+
+  const filterBy = (cat) => {
+    if (cat === "*") {
+      setActiveLink("*");
       setItems(designWorks1);
-   }, []);
+    } else {
+      const filteredItems = designWorks1.filter((item) => {
+        return item.categories.indexOf(cat) !== -1;
+      });
+      setActiveLink(cat);
+      setItems(filteredItems);
+    }
+  };
 
-   const filterBy = (cat) => {
-      if (cat === "*") {
-         setActiveLink("*");
-         setItems(designWorks1);
-      } else {
-         const filteredItems = designWorks1.filter((item) => {
-            return item.categories.indexOf(cat) !== -1;
-         });
-         setActiveLink(cat);
-         setItems(filteredItems);
-      }
-   };
+  const masonryOptions = {
+    transitionDuration: 1000,
+  };
+  return (
+    <div>
+      {/* <!-- Works Area --> */}
 
-   const masonryOptions = {
-      transitionDuration: 1000,
-   };
-   return (
-      <div style={{ backgroundColor: "#e6e6e6" }}>
-         {/* <!-- Works Area --> */}
-
-         <div
-            style={{
-               maxWidth: "1920px",
-               justifyContent: "center",
-               margin: "auto",
-               marginLeft: "100px",
-               marginRight: "100px",
-            }}
-         >
-            <div className="flexCenter">
-               <Slider comps={cards} length={5} title={"Бүтээгдэхүүн"} />
-            </div>
-         </div>
-
-         <img src={cityImg} className="my-4" />
-
-         <Center>
-            <div className="grid grid-cols-3 gap-8">
-               <Card />
-               <Card />
-               <Card />
-            </div>
-         </Center>
-
-         <Section className="position-relative">
-            <Container>
-               <Box mb="2.5rem" ml="-1.75rem">
-                  <ListNav className="nav">
-                     <li className="nav-item">
-                        <a
-                           className={`nav-link font-weight-bold text-uppercase ${
-                              activeLink === "*" ? "active" : null
-                           }`}
-                           onClick={(e) => {
-                              e.preventDefault();
-                              filterBy("*");
-                           }}
-                        >
-                           Бүх ажилууд
-                        </a>
-                     </li>
-                     <li className="nav-item">
-                        <a
-                           className={`nav-link font-weight-bold text-uppercase ${
-                              activeLink === "branding" ? "active" : null
-                           }`}
-                           onClick={(e) => {
-                              e.preventDefault();
-                              filterBy("branding");
-                           }}
-                        >
-                           Бренд
-                        </a>
-                     </li>
-                     <li className="nav-item">
-                        <a
-                           className={`nav-link font-weight-bold text-uppercase ${
-                              activeLink === "ux-design" ? "active" : null
-                           }`}
-                           onClick={(e) => {
-                              e.preventDefault();
-                              filterBy("ux-design");
-                           }}
-                        >
-                           дизайн
-                        </a>
-                     </li>
-                     <li className="nav-item">
-                        <a
-                           className={`nav-link font-weight-bold text-uppercase ${
-                              activeLink === "photography" ? "active" : null
-                           }`}
-                           onClick={(e) => {
-                              e.preventDefault();
-                              filterBy("photography");
-                           }}
-                        >
-                           График
-                        </a>
-                     </li>
-                  </ListNav>
-               </Box>
-            </Container>
-
-            <Container fluid>
-               <Masonry
-                  options={masonryOptions}
-                  className={"masonry-grid row"} // default ''
-               >
-                  {items.map((item, index) => (
-                     <Col
-                        lg="3"
-                        md="4"
-                        sm="6"
-                        key={index}
-                        className="filtr-item"
-                     >
-                        <WorkCard workItem={item} link={item.link} mb="30px" />
-                     </Col>
-                  ))}
-               </Masonry>
-            </Container>
-         </Section>
-         {/* <Footer /> */}
+      <div
+        style={{
+          maxWidth: "1920px",
+          justifyContent: "center",
+          margin: "auto",
+          marginLeft: "100px",
+          marginRight: "100px",
+        }}
+      >
+        <div className="flexCenter">
+          <Slider comps={cards} length={5} title={"Бүтээгдэхүүн"} />
+        </div>
       </div>
-   );
+
+      <img src={cityImg} className="my-4" />
+
+      <Center>
+        <div className="grid grid-cols-3 gap-8">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </Center>
+
+      <Section className="position-relative">
+        <Container>
+          <Box mb="2.5rem" ml="-1.75rem">
+            <ListNav className="nav">
+              <li className="nav-item">
+                <a
+                  className={`nav-link font-weight-bold text-uppercase ${
+                    activeLink === "*" ? "active" : null
+                  }`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    filterBy("*");
+                  }}
+                >
+                  Бүх ажилууд
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`nav-link font-weight-bold text-uppercase ${
+                    activeLink === "branding" ? "active" : null
+                  }`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    filterBy("branding");
+                  }}
+                >
+                  Бренд
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`nav-link font-weight-bold text-uppercase ${
+                    activeLink === "ux-design" ? "active" : null
+                  }`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    filterBy("ux-design");
+                  }}
+                >
+                  дизайн
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`nav-link font-weight-bold text-uppercase ${
+                    activeLink === "photography" ? "active" : null
+                  }`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    filterBy("photography");
+                  }}
+                >
+                  График
+                </a>
+              </li>
+            </ListNav>
+          </Box>
+        </Container>
+
+        <Container fluid>
+          <Masonry
+            options={masonryOptions}
+            className={"masonry-grid row"} // default ''
+          >
+            {items.map((item, index) => (
+              <Col lg="3" md="4" sm="6" key={index} className="filtr-item">
+                <WorkCard workItem={item} link={item.link} mb="30px" />
+              </Col>
+            ))}
+          </Masonry>
+        </Container>
+      </Section>
+      {/* <Footer /> */}
+    </div>
+  );
 };
 
 export default Works;
